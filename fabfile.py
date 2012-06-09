b54d1747c83c26bf30a40ec5239d9aa6e1517ab5
Leon Coto <leoncoto@gmail.com>
2012-06-09 17:36:41 +0100

    Adding check before pushing changes

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-from fabric.api import local
+from fabric.api import local, abort
+from fabric.contrib.console import confirm
 
 def commit():
     local('git add -p && git commit')
 
 def push():
+    if not confirm('Do you really want to push your changes?'):
+        abort('Aborting at user request')
     local('git push -u origin master')
 
 def cp():
